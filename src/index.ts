616cf42f27fdc7a96a251e718a4745ba457f313c
akikiya <2775686007@qq.com>
2025-02-07 11:00:22 +0800

    feat: add data-structure module and update docs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 /**
  * `@akey/tolkit` is designed to provide a collection of useful tools and utilities for JavaScript and TypeScript developers.
+ *
+ * Docs:
+ * [Array](https://jsr.io/@akey/tolkit/doc/array/)
+ * [Data Structure](https://jsr.io/@akey/tolkit/doc/data-structure/)
+ *
  * @module PackageIndex
  */
 
 export * from "./array/index.ts"
+export * from "./data-structure/index.ts"
