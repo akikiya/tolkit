deab98ffd13752cc2ffc6e87235a8610ee4c82f7
akikiya <2775686007@qq.com>
2025-02-12 14:37:35 +0800

    feat(data-structure):  update tests

--- src/data-structure/linked-list.test.ts
+++ src/data-structure/linked-list.test.ts
@@ -1,110 +1,122 @@
-import { assertEquals } from "@std/assert"
+import { assertEquals, assertThrows } from "@std/assert"
 import { LinkedList } from "./linked-list.ts"
 
-Deno.test("LinkedList prepend", () => {
+Deno.test("LinkedList prepend method", () => {
   const list = new LinkedList<number>()
-  list.prepend(1, 2, 3)
+  list.prepend(3, 2, 1)
   assertEquals(list.size(), 3)
-  assertEquals(list.get(0), 1)
+  assertEquals(list.get(0), 3)
   assertEquals(list.get(1), 2)
-  assertEquals(list.get(2), 3)
+  assertEquals(list.get(2), 1)
 })
 
-Deno.test("LinkedList append", () => {
-  const list = new LinkedList<number>()
-  list.append(1, 2, 3)
+Deno.test("LinkedList append method", () => {
+  const list = new LinkedList<string>()
+  list.append("a", "b", "c")
   assertEquals(list.size(), 3)
-  assertEquals(list.get(0), 1)
-  assertEquals(list.get(1), 2)
-  assertEquals(list.get(2), 3)
+  assertEquals(list.get(0), "a")
+  assertEquals(list.get(1), "b")
+  assertEquals(list.get(2), "c")
 })
 
-Deno.test("LinkedList insert", () => {
+Deno.test("LinkedList insert method", () => {
   const list = new LinkedList<number>()
-  list.append(1, 2, 3)
-  list.insert(1, 4, 5)
+  list.append(1, 2, 4, 5)
+  list.insert(2, 3)
   assertEquals(list.size(), 5)
-  assertEquals(list.get(0), 1)
-  assertEquals(list.get(1), 4)
-  assertEquals(list.get(2), 5)
-  assertEquals(list.get(3), 2)
-  assertEquals(list.get(4), 3)
-})
-
-Deno.test("LinkedList get", () => {
-  const list = new LinkedList<number>()
-  list.append(1, 2, 3)
-  assertEquals(list.get(0), 1)
-  assertEquals(list.get(1), 2)
   assertEquals(list.get(2), 3)
+  assertEquals(list.toArray(), [1, 2, 3, 4, 5])
+
+  list.insert(0, 0)
+  assertEquals(list.get(0), 0)
+  assertEquals(list.size(), 6)
+
+  list.insert(6, 6)
+  assertEquals(list.get(6), 6)
+  assertEquals(list.size(), 7)
+
+  assertThrows(() => list.insert(8, 8), RangeError)
 })
 
-Deno.test("LinkedList set", () => {
+Deno.test("LinkedList get method", () => {
+  const list = new LinkedList<number>()
+  list.append(10, 20, 30)
+  assertEquals(list.get(0), 10)
+  assertEquals(list.get(1), 20)
+  assertEquals(list.get(2), 30)
+  assertThrows(() => list.get(3), RangeError)
+})
+
+Deno.test("LinkedList set method", () => {
   const list = new LinkedList<number>()
   list.append(1, 2, 3)
-  list.set(1, 4)
-  assertEquals(list.get(0), 1)
-  assertEquals(list.get(1), 4)
-  assertEquals(list.get(2), 3)
+  list.set(1, 20)
+  assertEquals(list.get(1), 20)
+  assertThrows(() => list.set(3, 0), RangeError)
 })
 
-Deno.test("LinkedList indexOf", () => {
+Deno.test("LinkedList indexOf method", () => {
   const list = new LinkedList<number>()
-  list.append(1, 2, 3)
-  assertEquals(list.indexOf(2), 1)
-  assertEquals(list.indexOf(4), null)
+  list.append(5, 10, 15, 20)
+  assertEquals(list.indexOf(10), 1)
+  assertEquals(list.indexOf(20), 3)
+  assertEquals(list.indexOf(25), null)
 })
 
-Deno.test("LinkedList clear", () => {
+Deno.test("LinkedList delete method", () => {
+  const list = new LinkedList<string>()
+  list.append("a", "b", "c", "d", "e")
+  list.delete(2)
+  assertEquals(list.size(), 4)
+  assertEquals(list.get(2), "d")
+  assertThrows(() => list.delete(4), RangeError)
+
+  list.delete(0)
+  assertEquals(list.get(0), "b")
+  assertEquals(list.size(), 3)
+
+  list.delete(2)
+  assertEquals(list.size(), 2)
+  assertEquals(list.get(1), "d")
+})
+
+Deno.test("LinkedList clear method", () => {
   const list = new LinkedList<number>()
   list.append(1, 2, 3)
   list.clear()
   assertEquals(list.size(), 0)
 })
 
-Deno.test("LinkedList delete", () => {
-  const list = new LinkedList<number>()
-  list.append(1, 2, 3)
-  list.delete(1)
-  assertEquals(list.size(), 2)
-  assertEquals(list.get(0), 1)
-  assertEquals(list.get(1), 3)
+Deno.test("LinkedList merge static method", () => {
+  const list1 = new LinkedList<number>()
+  list1.append(1, 2, 3)
+  const list2 = new LinkedList<number>()
+  list2.append(4, 5, 6)
+  const mergedList = LinkedList.merge(list1, list2)
+  assertEquals(mergedList.size(), 6)
+  assertEquals(mergedList.toArray(), [1, 2, 3, 4, 5, 6])
 })
 
-Deno.test("LinkedList size", () => {
+Deno.test("LinkedList filter static method", () => {
   const list = new LinkedList<number>()
-  list.append(1, 2, 3)
-  assertEquals(list.size(), 3)
+  list.append(1, 2, 3, 4, 5, 6)
+  const filteredList = LinkedList.filter(list, value => value % 2 === 0)
+  assertEquals(filteredList.size(), 3)
+  assertEquals(filteredList.toArray(), [2, 4, 6])
 })
 
 Deno.test("LinkedList iterator", () => {
   const list = new LinkedList<number>()
-  list.append(1, 2, 3)
-  const iterator = list[Symbol.iterator]()
-  assertEquals(iterator.next().value, 1)
-  assertEquals(iterator.next().value, 2)
-  assertEquals(iterator.next().value, 3)
-  assertEquals(iterator.next().done, true)
-})
-
-Deno.test("LinkedList merge", () => {
-  const list1 = new LinkedList<number>()
-  list1.append(1, 2)
-  const list2 = new LinkedList<number>()
-  list2.append(3, 4)
-  const mergedList = LinkedList.merge(list1, list2)
-  assertEquals(mergedList.size(), 4)
-  assertEquals(mergedList.get(0), 1)
-  assertEquals(mergedList.get(1), 2)
-  assertEquals(mergedList.get(2), 3)
-  assertEquals(mergedList.get(3), 4)
-})
-
-Deno.test("LinkedList filter", () => {
-  const list = new LinkedList<number>()
   list.append(1, 2, 3, 4, 5)
-  const filteredList = LinkedList.filter(list, value => value % 2 === 0)
-  assertEquals(filteredList.size(), 2)
-  assertEquals(filteredList.get(0), 2)
-  assertEquals(filteredList.get(1), 4)
+  const values: number[] = []
+  for (const value of list) {
+    values.push(value)
+  }
+  assertEquals(values, [1, 2, 3, 4, 5])
+})
+
+Deno.test("LinkedList toArray method", () => {
+  const list = new LinkedList<number>()
+  list.append(1, 2, 3)
+  assertEquals(list.toArray(), [1, 2, 3])
 })
--- src/data-structure/queue.test.ts
+++ src/data-structure/queue.test.ts
@@ -1,50 +1,106 @@
-import { assertEquals } from "@std/assert"
+import { assertEquals, assertThrows } from "@std/assert"
 import { LinkedListQueue } from "./queue.ts"
 
-Deno.test("LinkedListQueue enqueue", () => {
+Deno.test("LinkedListQueue size method", () => {
   const queue = new LinkedListQueue<number>()
+  assertEquals(queue.size(), 0)
   queue.enqueue(1, 2, 3)
   assertEquals(queue.size(), 3)
-  assertEquals(queue.front(), 1)
-  assertEquals(queue.back(), 3)
-})
-
-Deno.test("LinkedListQueue dequeue", () => {
-  const queue = new LinkedListQueue<number>()
-  queue.enqueue(1, 2, 3)
-  assertEquals(queue.dequeue(), 1)
+  queue.dequeue()
   assertEquals(queue.size(), 2)
-  assertEquals(queue.front(), 2)
-})
-
-Deno.test("LinkedListQueue front", () => {
-  const queue = new LinkedListQueue<number>()
-  queue.enqueue(1, 2, 3)
-  assertEquals(queue.front(), 1)
-})
-
-Deno.test("LinkedListQueue back", () => {
-  const queue = new LinkedListQueue<number>()
-  queue.enqueue(1, 2, 3)
-  assertEquals(queue.back(), 3)
-})
-
-Deno.test("LinkedListQueue clear", () => {
-  const queue = new LinkedListQueue<number>()
-  queue.enqueue(1, 2, 3)
   queue.clear()
   assertEquals(queue.size(), 0)
 })
 
-Deno.test("LinkedListQueue toArray", () => {
+Deno.test("LinkedListQueue isEmpty method", () => {
+  const queue = new LinkedListQueue<string>()
+  assertEquals(queue.isEmpty(), true)
+  queue.enqueue("a")
+  assertEquals(queue.isEmpty(), false)
+  queue.dequeue()
+  assertEquals(queue.isEmpty(), true)
+})
+
+Deno.test("LinkedListQueue enqueue method", () => {
   const queue = new LinkedListQueue<number>()
   queue.enqueue(1, 2, 3)
   assertEquals(queue.toArray(), [1, 2, 3])
+  queue.enqueue(4, 5)
+  assertEquals(queue.toArray(), [1, 2, 3, 4, 5])
 })
 
-Deno.test("LinkedListQueue isEmpty", () => {
-  const queue = new LinkedListQueue<number>()
+Deno.test("LinkedListQueue dequeue method", () => {
+  const queue = new LinkedListQueue<string>()
+  queue.enqueue("a", "b", "c")
+  assertEquals(queue.dequeue(), "a")
+  assertEquals(queue.dequeue(), "b")
+  assertEquals(queue.dequeue(), "c")
+  assertEquals(queue.dequeue(), undefined)
   assertEquals(queue.isEmpty(), true)
-  queue.enqueue(1)
-  assertEquals(queue.isEmpty(), false)
+})
+
+Deno.test("LinkedListQueue front method", () => {
+  const queue = new LinkedListQueue<number>()
+  assertThrows(() => queue.front(), RangeError)
+  queue.enqueue(10, 20, 30)
+  assertEquals(queue.front(), 10)
+  queue.dequeue()
+  assertEquals(queue.front(), 20)
+  queue.clear()
+  assertThrows(() => queue.front(), RangeError)
+})
+
+Deno.test("LinkedListQueue back method", () => {
+  const queue = new LinkedListQueue<string>()
+  assertThrows(() => queue.back(), RangeError)
+  queue.enqueue("x", "y", "z")
+  assertEquals(queue.back(), "z")
+  queue.enqueue("w")
+  assertEquals(queue.back(), "w")
+  queue.dequeue()
+  assertEquals(queue.back(), "w")
+  queue.clear()
+  assertThrows(() => queue.back(), RangeError)
+})
+
+Deno.test("LinkedListQueue clear method", () => {
+  const queue = new LinkedListQueue<number>()
+  queue.enqueue(1, 2, 3)
+  queue.clear()
+  assertEquals(queue.isEmpty(), true)
+  assertEquals(queue.size(), 0)
+  assertEquals(queue.toArray(), [])
+})
+
+Deno.test("LinkedListQueue toArray method", () => {
+  const queue = new LinkedListQueue<number>()
+  assertEquals(queue.toArray(), [])
+  queue.enqueue(5, 10, 15)
+  assertEquals(queue.toArray(), [5, 10, 15])
+  queue.dequeue()
+  assertEquals(queue.toArray(), [10, 15])
+  queue.enqueue(20)
+  assertEquals(queue.toArray(), [10, 15, 20])
+})
+
+Deno.test("LinkedListQueue dequeue from empty queue", () => {
+  const queue = new LinkedListQueue<number>()
+  assertEquals(queue.dequeue(), undefined)
+})
+
+Deno.test("LinkedListQueue enqueue multiple elements", () => {
+  const queue = new LinkedListQueue<number>()
+  queue.enqueue(1, 2, 3, 4, 5)
+  assertEquals(queue.toArray(), [1, 2, 3, 4, 5])
+})
+
+Deno.test("LinkedListQueue dequeue and enqueue", () => {
+  const queue = new LinkedListQueue<number>()
+  queue.enqueue(1, 2, 3)
+  assertEquals(queue.dequeue(), 1)
+  assertEquals(queue.toArray(), [2, 3])
+  queue.enqueue(4, 5)
+  assertEquals(queue.toArray(), [2, 3, 4, 5])
+  assertEquals(queue.dequeue(), 2)
+  assertEquals(queue.toArray(), [3, 4, 5])
 })
